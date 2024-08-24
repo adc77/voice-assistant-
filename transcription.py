057dc4aca7f5d6a56dd9a50e799d2666527bfc79
adc77 <amandogra2016@gmail.com>
2024-08-24 13:13:06 +0530

    stop tracking myenv

---
+++ transcription.py
@@ -0,0 +1,30 @@
+import os
+from faster_whisper import WhisperModel
+
+# Create transcriptions directory if it doesn't exist
+if not os.path.exists("transcriptions"):
+    os.makedirs("transcriptions")
+
+model_size = "large-v3"
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+audio_file = r"recordings/audio1.mp3"
+segments, info = model.transcribe(audio_file, beam_size=5)
+
+print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+
+for segment in segments:
+    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+
+segments, _ = model.transcribe(audio_file)
+segments = list(segments)  # The transcription will actually run here.
+
+# Save transcription to a file
+transcription_index = 1  # Start indexing from 1
+transcription_file = f"transcriptions/transcription{transcription_index}.txt"
+
+with open(transcription_file, "w") as f:
+    for segment in segments:
+        f.write(f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}\n")
+
+print(f"Transcription saved to {transcription_file}")
